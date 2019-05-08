0ad99600af083b1c6d585221e9b8688afa933310
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2019-05-08 14:04:03 +0800

    edits

--- source/conf.py
+++ source/conf.py
@@ -44,7 +44,7 @@ templates_path = ['_templates']
 source_suffix = '.rst'
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = 'intro'
 
 # General information about the project.
 project = u'Ubuntu Admin Recipes'
